7d5cd786c313374f6579e7951017ab6ff70035be
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-05 13:55:04 +0500

    Task 3 completed

---
+++ q3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Ali's Balcony Design\n\n";
+    cout << setfill('=') << setw(25) << "=" << endl;
+    cout << "  /\\    /\\    /\\    /\\   " << endl;
+    cout << " /  \\  /  \\  /  \\  /  \\  " << endl;
+    cout << " |  |  |  |  |  |  |  |  " << endl;
+    cout << " \\  /  \\  /  \\  /  \\  /  " << endl;
+    cout << "  \\/    \\/    \\/    \\/   " << endl;
+    cout << setfill('=') << setw(25) << "=" << endl;
+
+    return 0;
+}
