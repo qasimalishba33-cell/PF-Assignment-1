87fb44d01bff4906e75135e7cec982900868b86e
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-05 13:53:33 +0500

    Task 1 completed

---
+++ q1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    cout << setfill(' ');
+
+
+    cout << '\n';
+
+
+    cout << setw(36) << "   /\\   " << '\n';
+    cout << setw(36) << "  /  \\  " << '\n';
+    cout << setw(36) << " /----\\ " << '\n';
+    cout << setw(36) << "/######\\" << '\n';
+
+ 
+    cout << setw(34) << "  ____" <<  '\n';
+    cout << setw(35) << " |____|" << '\n';
+
+ 
+    cout << setw(41) << " /-------------\\ " << '\n';
+    cout << setw(42) << "/===============\\ " << '\n';
+
+  
+    cout << setw(41) << "|--------------|" << '\n';
+
+   
+    cout << setw(25) << " " << setw(8) << "|  [ ]  " << setw(8) << " [ ]  |" << '\n';
+    cout << setw(25) << " " << setw(8) << "|   |   " << setw(8) << "  |   |" << '\n';
+
+    
+    cout << setw(23) << " " << setw(18) << "|  ___   ____  |" << '\n';
+    cout << setw(23) << " " << setw(18) << "||    | |    | |" << '\n';
+    cout << setw(23) << " " << setw(18) << " ||____| |____| |" << '\n';
+
+    
+    cout << setw(23) << " " << setw(18) << "|              |" << '\n';
+
+
+    cout << setw(25) << " " << setw(14) << "|    __   __   |" << '\n';
+    cout << setw(25) << " " << setw(14) << "|   |  | |  |  |" << '\n';
+    cout << setw(25) << " " << setw(14) << "|   |  | |  |  |" << '\n';
+    cout << setw(25) << " " << setw(14) << "|   |__| |__|  |" << '\n';
+
+
+    cout << setw(23) << " " << setw(18) << "|______________|" << '\n';
+
+    
+
+    cout << '\n';
+    return 0;
+}
