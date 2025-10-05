2762ceff6159915f1bf303b4bac151c6c725386b
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-05 13:55:39 +0500

    Task 4 completed

---
+++ q4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "  ____________________  \n"
+            " |                    | \n"
+            " |    ************    | \n"
+            " |    *          *    | \n"
+            " |    *  ALI     *    | \n"
+            " |    *  PHONE   *    | \n"
+            " |    *__________*    | \n"
+            " |                    | \n"
+            " |   [   MENU   ]     | \n"
+            " |   [ CONTACTS ]     | \n"
+            " |   [  BROWSER ]     | \n"
+            " |   [ SETTINGS ]     | \n"
+            " |                    | \n"
+            " |____________________| \n"
+            "     ( )        ( )     \n";
+    return 0;
+}
